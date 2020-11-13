d6ccf31ab4160d3fa3610c9e033059cdcf75b275
Bruno <56009821+GiaconBruno@users.noreply.github.com>
2020-11-12 23:41:08 -0300

    Responsive

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ new Vue({
         atkMonstro: [],
         resultado: 'Jogando',
         fim: false,
+        stop: true,
     },
     computed: {
         playerHealth() {
@@ -41,11 +42,12 @@ new Vue({
             this.jogadas = 0;
             this.start = true;
             this.resultado = 'Jogando';
+            this.stop = true;
             this.fim = false;
         },
         ataque() {
             const danoPlayer = parseInt(Math.random() * (7 - 1) + 1);
-            const danoMonstro = parseInt(Math.random() * (10 - 1) + 1);
+            const danoMonstro = parseInt(Math.random() * (11 - 1) + 1);
             this.curaPlayer.push(0);
             this.player - danoMonstro <= 0 ? this.player = 0 : this.player -= danoMonstro;
             this.atkMonstro.push(danoMonstro);
@@ -56,7 +58,7 @@ new Vue({
             // console.log(danoPlayer, danoMonstro);
         },
         skill() {
-            const skillPlayer = parseInt(Math.random() * (10 - 1) + 1);
+            const skillPlayer = parseInt(Math.random() * (11 - 1) + 1);
             const danoMonstro = parseInt(Math.random() * (7 - 1) + 1);
             this.curaPlayer.push(0);
             this.player - danoMonstro <= 0 ? this.player = 0 : this.player -= danoMonstro;
@@ -68,8 +70,8 @@ new Vue({
             // console.log(skillPlayer, danoMonstro);
         },
         heal() {
-            const healPlayer = parseInt(Math.random() * (10 - 1) + 1);
-            const danoMonstro = parseInt(Math.random() * (10 - 1) + 1);
+            const healPlayer = parseInt(Math.random() * (11 - 1) + 1);
+            const danoMonstro = parseInt(Math.random() * (11 - 1) + 1);
             this.atkPlayer.push(0);
             this.player - danoMonstro < 0 ? this.player = 0 : this.player -= danoMonstro;
             this.atkMonstro.push(danoMonstro);
@@ -78,6 +80,19 @@ new Vue({
             this.jogadas++;
             this.end();
             // console.log(healPlayer, danoMonstro);
+        },
+        auto() {
+            this.stop = !this.stop;
+            const timer = setInterval(() => {
+                const play = parseInt(Math.random() * (30 - 1) + 1);
+                if ((this.resultado == 'Jogando') && (this.stop == false)) {
+                    if (play <= 10) this.ataque();
+                    else if ((play > 10) && (play <= 20)) this.skill();
+                    else this.heal();
+                    this.end();
+                    // console.log('-> ', play);
+                } else clearInterval(timer);
+            }, 500);
         },
         end() {
             if (this.player == 0) {
